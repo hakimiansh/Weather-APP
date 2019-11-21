954c05082e09466952534b44828e6f35d27d61ef
Shani Staretz <shakimi88@gmail.com>
2019-11-21 13:38:50 +0200

    Auto-generated commit

---
+++ assets/weatherIcons.ts
@@ -0,0 +1,200 @@
+export var ICONS=[{
+	icon_id: 1,
+	imgSrc: "https://developer.accuweather.com/sites/default/files/01-s.png",
+	text:"Sunny"
+},
+{
+	icon_id: 2,
+	imgSrc: "https://developer.accuweather.com/sites/default/files/02-s.png",
+	text:"Mostly Sunny"
+},
+{
+	icon_id: 3,
+	imgSrc: "https://developer.accuweather.com/sites/default/files/03-s.png",
+	text:"Partly Sunny"
+},
+{
+	icon_id: 4,
+	imgSrc: "https://developer.accuweather.com/sites/default/files/04-s.png",
+	text:"Intermittent Clouds"
+},
+{
+	icon_id: 5,
+	imgSrc: "https://developer.accuweather.com/sites/default/files/05-s.png",
+	text:"Hazy Sunshine"
+},
+{
+	icon_id: 6,
+	imgSrc: "https://developer.accuweather.com/sites/default/files/06-s.png",
+	text:"Mostly Cloudy"
+},
+{
+	icon_id: 7,
+	imgSrc: "https://developer.accuweather.com/sites/default/files/07-s.png",
+	text:"Cloudy"
+},
+{
+	icon_id: 8,
+	imgSrc: "https://developer.accuweather.com/sites/default/files/08-s.png",
+	text:"Dreary (Overcast)"
+},
+{
+	icon_id: 11,
+	imgSrc: "https://developer.accuweather.com/sites/default/files/11-s.png",
+	text:"Fog"
+},
+{
+	icon_id: 12,
+	imgSrc: "https://developer.accuweather.com/sites/default/files/12-s.png",
+	text:"Showers"
+},
+{
+	icon_id: 13,
+	imgSrc: "https://developer.accuweather.com/sites/default/files/13-s.png",
+	text:"Mostly Cloudy w/ Showers"
+},
+{
+	icon_id: 14,
+	imgSrc: "https://developer.accuweather.com/sites/default/files/14-s.png",
+	text:"Partly Sunny w/ Showers"
+},
+{
+	icon_id: 15,
+	imgSrc: "https://developer.accuweather.com/sites/default/files/15-s.png",
+	text:"T-Storms"
+},
+{
+	icon_id: 16,
+	imgSrc: "https://developer.accuweather.com/sites/default/files/16-s.png",
+	text:"Mostly Cloudy w/ T-Storms"
+},
+{
+	icon_id: 17,
+	imgSrc: "https://developer.accuweather.com/sites/default/files/17-s.png",
+	text:"Partly Sunny w/ T-Storms"
+},
+{
+	icon_id: 18,
+	imgSrc: "https://developer.accuweather.com/sites/default/files/18-s.png",
+	text:"Rain"
+},
+{
+	icon_id: 19,
+	imgSrc: "https://developer.accuweather.com/sites/default/files/19-s.png",
+	text:"Flurries"
+},
+{
+	icon_id: 20,
+	imgSrc: "https://developer.accuweather.com/sites/default/files/20-s.png",
+	text:"Mostly Cloudy w/ Flurries"
+},
+{
+	icon_id: 21,
+	imgSrc: "https://developer.accuweather.com/sites/default/files/21-s.png",
+	text:"Partly Sunny w/ Flurries"
+},
+{
+	icon_id: 22,
+	imgSrc: "https://developer.accuweather.com/sites/default/files/22-s.png",
+	text:"Snow"
+},
+{
+	icon_id: 23,
+	imgSrc: "https://developer.accuweather.com/sites/default/files/23-s.png",
+	text:"Mostly Cloudy w/ Snow"
+},
+{
+	icon_id: 24,
+	imgSrc: "https://developer.accuweather.com/sites/default/files/24-s.png",
+	text:"Ice"
+},
+{
+	icon_id: 25,
+	imgSrc: "https://developer.accuweather.com/sites/default/files/25-s.png",
+	text:"Sleet"
+},
+{
+	icon_id: 26,
+	imgSrc: "https://developer.accuweather.com/sites/default/files/26-s.png",
+	text:"Freezing Rain"
+},
+{
+	icon_id: 29,
+	imgSrc: "https://developer.accuweather.com/sites/default/files/29-s.png",
+	text:"Rain and Snow"
+},
+{
+	icon_id: 30,
+	imgSrc: "https://developer.accuweather.com/sites/default/files/30-s.png",
+	text:"Hot"
+},
+{
+	icon_id: 31,
+	imgSrc: "https://developer.accuweather.com/sites/default/files/31-s.png",
+	text:"Cold"
+},
+{
+	icon_id: 32,
+	imgSrc: "https://developer.accuweather.com/sites/default/files/32-s.png",
+	text:"Windy"
+},
+{
+	icon_id: 33,
+	imgSrc: "https://developer.accuweather.com/sites/default/files/33-s.png",
+	text:"Clear"
+},
+{
+	icon_id: 34,
+	imgSrc: "https://developer.accuweather.com/sites/default/files/34-s.png",
+	text:"Mostly Clear"
+},
+{
+	icon_id: 35,
+	imgSrc: "https://developer.accuweather.com/sites/default/files/35-s.png",
+	text:"Partly Cloudy"
+},
+{
+	icon_id: 36,
+	imgSrc: "https://developer.accuweather.com/sites/default/files/36-s.png",
+	text:"Intermittent Clouds"
+},
+{
+	icon_id: 37,
+	imgSrc: "https://developer.accuweather.com/sites/default/files/37-s.png",
+	text:"Hazy Moonlight"
+},
+{
+	icon_id: 38,
+	imgSrc: "https://developer.accuweather.com/sites/default/files/38-s.png",
+	text:"Mostly Cloudy"
+},
+{
+	icon_id: 39,
+	imgSrc: "https://developer.accuweather.com/sites/default/files/39-s.png",
+	text:"Partly Cloudy w/ Showers"
+},
+{
+	icon_id: 40,
+	imgSrc: "https://developer.accuweather.com/sites/default/files/40-s.png",
+	text:"Mostly Cloudy w/ Showers"
+},
+{
+	icon_id: 41,
+	imgSrc: "https://developer.accuweather.com/sites/default/files/41-s.png",
+	text:"Partly Cloudy w/ T-Storms"
+},
+{
+	icon_id: 42,
+	imgSrc: "https://developer.accuweather.com/sites/default/files/42-s.png",
+	text:"Mostly Cloudy w/ T-Storms"
+},
+{
+	icon_id: 43,
+	imgSrc: "https://developer.accuweather.com/sites/default/files/43-s.png",
+	text:"Mostly Cloudy w/ Flurries"
+},
+{
+	icon_id: 44,
+	imgSrc: "https://developer.accuweather.com/sites/default/files/44-s.png",
+	text:"Mostly Cloudy w/ Snow"
+}]
